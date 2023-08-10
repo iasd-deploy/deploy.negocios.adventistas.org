30a5fdccbe8e3f242b65f4538d54c51e60145f21
Mizael Isaltino <mizael.isaltino@gmail.com>
2023-08-10 18:03:10 -0300

    teste 5

---
+++ app/wp-content/themes/pa-theme-negocios/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'igrejaadventista/pa-theme-negocios',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e27e73ad9df6ca576d4955318b0e98b00434f059',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'igrejaadventista/pa-theme-negocios' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e27e73ad9df6ca576d4955318b0e98b00434f059',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wordplate/acf' => array(
+            'pretty_version' => '13.6.0',
+            'version' => '13.6.0.0',
+            'reference' => '98e5da266ecdb90e924928f3e04d1c92544a23e7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wordplate/acf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
