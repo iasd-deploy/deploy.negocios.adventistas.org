cd7facffc018cdcbfcfa34264f43668498db1c9b
clecyo.freitas <clecyo.freitas@adventistas.org>
2025-01-08 10:33:37 -0300

    configurando ci/cd

--- app/wp-config.php
+++ app/wp-config.php
@@ -21,16 +21,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', $_ENV['WP_DB_NAME']);
+define('DB_NAME', 'WP_DB_NAME');
 
-/** MySQL database username */
-define('DB_USER', $_ENV['WP_DB_USER']);
+/** Tu nombre de usuario de MySQL */
+define('DB_USER', 'WP_DB_USER');
 
-/** MySQL database password */
-define('DB_PASSWORD', $_ENV['WP_DB_PASSWORD']);
+/** Tu contraseña de MySQL */
+define('DB_PASSWORD', 'WP_DB_PASSWORD');
 
-/** MySQL hostname */
-define('DB_HOST', $_ENV['WP_DB_HOST'] . ':3306');
+/** Host de MySQL (es muy probable que no necesites cambiarlo) */
+define('DB_HOST', 'WP_DB_HOST');
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
@@ -40,9 +40,9 @@ define('DB_COLLATE', '');
 
 define('AS3CF_SETTINGS', serialize(array(
 	'provider' => 'aws',
-	'access-key-id' => $_ENV['WP_S3_ACCESS_KEY'],
-	'secret-access-key' => $_ENV['WP_S3_SECRET_KEY'],
-	'bucket' => $_ENV['WP_S3_BUCKET'],
+    'access-key-id' => 'WP_S3_ACCESS_KEY',
+    'secret-access-key' => 'WP_S3_SECRET_KEY',
+	'bucket' => 'WP_S3_BUCKET',
 	'enable-delivery-domain' => true,
 	'delivery-domain' => 'files.adventistas.org'
 )));
